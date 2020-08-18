56efc8810dd1da2241c7df8f44b92a262c502505
Muhammed <muhammed.uk@kreatio.com>
2020-08-19 01:08:31 +0530

    Muhammed | handling record not found expection and request spec

--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -2,10 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/articles/index"
-      expect(response).to have_http_status(:success)
+  before do
+    @article = Article.create(title: "1st Article", body: "Content for 1st article")
+  end
+
+  describe 'GET /articles/:id' do
+    context 'with existing article' do
+      before { get article_path(@article) }
+      it 'handles existing article' do
+        expect(response.status).to eq(200)
+      end
+    end
+    context 'with non-existing article' do
+      before { get '/articles/xxx' }
+      it 'handles non-existing article' do
+        expect(response.status).to eq(302)
+        flash_message = "The article you are looking for could not be found"
+        expect(flash[:alert]).to eq(flash_message)
+      end
     end
   end
 
