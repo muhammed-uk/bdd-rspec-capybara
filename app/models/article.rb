67b65cedfcbcbf4ebb78caed803cd1990df053b8
Muhammed <muhammed.uk@kreatio.com>
2020-08-23 20:06:03 +0530

    create article modification: user required

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates_presence_of :title, :body
   default_scope { order(created_at: :desc) }
 end
--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature 'Creating Article' do
+
+  before do
+    @muhammed = User.create!(
+        email: "muhammed@uk.com",
+        password: "password"
+    )
+    login_as(@muhammed)
+  end
+
   scenario 'User create a new article' do
     visit root_path
     click_link 'New Article'
@@ -8,7 +17,9 @@ RSpec.feature 'Creating Article' do
     fill_in 'Body', with: 'Lorum Ipsum'
     click_button 'Create Article'
 
+    expect(Article.last.user).to eq(@muhammed)
     expect(page).to have_content 'Article has been created'
     expect(page.current_path).to eq(articles_path)
+    expect(page).to have_content("Created by: #{@muhammed.email}")
   end
 end
